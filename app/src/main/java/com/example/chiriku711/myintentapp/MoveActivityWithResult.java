4383dbeb9a846df982d3616d1d471d90adc02454
alirhmat <chiriku.ali.rhmat@gmail.com>
2018-01-13 17:08:43 +0700

    add function intent  with result

---
+++ app/src/main/java/com/example/chiriku711/myintentapp/MoveActivityWithResult.java
@@ -0,0 +1,13 @@
+package com.example.chiriku711.myintentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoveActivityWithResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_result);
+    }
+}
